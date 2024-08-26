2cda1a816bd7af495dd8d6af810ea819dec2b1b4
arazamughal <ahmadraza89789@gmail.com>
2024-08-26 17:01:09 +0500

    products and customers pages

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+    /** @type {import('tailwindcss').Config} */
+    module.exports = {
+      
+      content: [
+        "/node_modules/flowbite/**/*.js",
+        "./**/*.{html,js}"],
+      theme: {
+        extend: {},
+      },
+      plugins: [
+        require('flowbite/plugin')({
+            datatables: true,
+        }),
+      ]
+    }
